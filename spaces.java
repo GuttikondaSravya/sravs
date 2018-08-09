0ac559c2b7d75d051830ed98a60e53935dc8f8a9
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-09 11:55:11 +0530

    Create spaces.java

---
+++ spaces.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int i,count=0;
+		String s="";
+		s+=sc.nextLine();
+		String[] words=s.split("\\s");
+		for(i=0;i<words.length;i++)
+		{
+			count++;
+		}
+		 System.out.print(count-1);
+	}
+}
