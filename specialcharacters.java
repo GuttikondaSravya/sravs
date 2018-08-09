bb20e5778cb4d4386966757a0512b9581118ce65
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-09 12:49:55 +0530

    Create specialcharacters.java

---
+++ specialcharacters.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+		public static void fun(String special)
+	{
+		String str;
+		int i,j,count=0;
+		Scanner sc=new Scanner(System.in);
+	//	System.out.println("enter the string:");
+		str=sc.nextLine();
+		for(i=0;i<str.length();i++)
+		{
+			for(j=0;j<special.length();j++)
+			{
+			
+				 if(str.charAt(i)==special.charAt(j))
+				{
+					count++;
+				}
+			
+			}
+		}
+		System.out.println(""+count);
+	}
