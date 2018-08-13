3f62851a39ff22af81a4fcd193d31e9f0abbca63
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-13 09:50:11 +0530

    Create 46p.java

---
+++ 46p.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+ class Nextnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,sum=0;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+sum=n+1;
+System.out.println(sum);
+	}
+
+}
