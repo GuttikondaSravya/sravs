d9bce19428bde9d6eff8e5b563e0bcd68afc4b2f
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-14 12:46:35 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class Ideone
+ {
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		float num=in.nextFloat();
+		int num1=Math.round(num);
+		System.out.println(num1+1);
+	}
+}
