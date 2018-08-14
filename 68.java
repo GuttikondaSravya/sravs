e42357d986c5a2c0ee6b9f3c1d2a635e932741ca
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-14 11:19:08 +0530

    Create 68.java

---
+++ 68.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ class Ideone
+ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int n;
+       Scanner ja=new Scanner(System.in);
+       n=ja.nextInt();
+       if(n%7==0)
+{
+	System.out.println("yes");
+}
+else
+{
+	System.out.println("no");
+}
+	}
+
+}
