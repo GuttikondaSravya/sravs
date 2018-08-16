5f4d450cb185d063b815d4b1bd5d5b09e64355ee
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-16 10:58:23 +0530

    Create 81.java

---
+++ 81.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Ideone
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int r=Math.abs(a-b);
+                System.out.print(r);
+    }    
+        
+}
