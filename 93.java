7cf1490a26913721a8f857d28818cd9fdee5f787
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-23 10:07:37 +0530

    Create 93.java

---
+++ 93.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class Ideone
+ {
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	 int temp=in.nextInt();
+	 float kelvin=273.15f;
+	 kelvin=kelvin+temp;
+	 System.out.println(kelvin);
+	}
+}
