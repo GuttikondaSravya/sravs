5516068a0286694f8aceb0c1f6a0d20e4483f087
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-23 09:57:04 +0530

    Update 82p.java

--- 82p.java
+++ 82p.java
@@ -8,11 +8,11 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		        float a,d;
+		        float b,c;
         Scanner s = new Scanner(System.in);
-        a= s.nextFloat();
-        d = s.nextFloat();
-                float res=a*d;
+        b= s.nextFloat();
+        c = s.nextFloat();
+                float res=b*c;
 	        System.out.format("%.5f", res);
 
                 
