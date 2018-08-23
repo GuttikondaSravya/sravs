6f3ac8a67981a04632bda28d537661980fe0e25b
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-23 12:26:14 +0530

    Create 105.java

---
+++ 105.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+class Ideone
+{
+
+	public static void main(String[] args) 
+	{
+Scanner in=new Scanner(System.in);
+String str=in.nextLine();
+String s=str.replaceAll(" ","");
+System.out.println(s);
+	}
+
+}
