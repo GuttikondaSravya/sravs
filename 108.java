9ac31e5b39cb7b0f4d7456aa636752f671f8e976
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-23 12:39:20 +0530

    Create 108.java

---
+++ 108.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ class Ideone
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int k=in.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=in.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.println(a[k-1]);
+		}
+}
