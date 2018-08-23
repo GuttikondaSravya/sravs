547257453e817315cdc9036bb0fbb00574cd1ce0
GuttikondaSravya <41734193+GuttikondaSravya@users.noreply.github.com>
2018-08-23 10:29:06 +0530

    Create 95.java

---
+++ 95.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class Ideone
+ {
+
+	public static void main(String[] args)
+	{
+              Scanner in=new Scanner(System.in);
+              int p=in.nextInt();
+              int t=in.nextInt();
+              int r=in.nextInt();
+              int si=(p*t*r)/100;
+              System.out.println(si);
+	}
+
+}
